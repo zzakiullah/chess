5d5a9a9dfc824eede77616a78ef0348d0dfc9de3
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2022-08-26 14:18:32 -0400

    Create basic window

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# chess
+# Chess Trainer
+
+Personal chess trainer using [Stockfish 15](https://github.com/official-stockfish/Stockfish/tree/sf_15).
--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,39 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-class Window extends JFrame {
+class Window extends JFrame implements ActionListener, KeyListener {
+
+    int width, height;
+
+    Timer myTimer;
+
     public Window() {
-        super();
+        super("Chess Trainer");
+        setIconImage(new ImageIcon(getClass().getResource("chess-knight-solid.png").getPath()).getImage());
+
+        width = 800;
+        height = 600;
+
+        setSize(width, height);
+
+        myTimer = new Timer(10, this);
+        myTimer.start();
+
+        addKeyListener(this);
+        requestFocus();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent e) {}
+
+    public void keyTyped(KeyEvent e) {}
+    public void keyPressed(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {}
+
 }
